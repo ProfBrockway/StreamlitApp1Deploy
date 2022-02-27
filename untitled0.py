b8594d9f8eec5fd70be6075b4c8765f097badad1
ProfBrockway <84090839+ProfBrockway@users.noreply.github.com>
2022-02-27 11:21:26 -0500

    c

--- untitled0.py
+++ untitled0.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
 url = "https://drive.google.com/file/d/1aE_7AUXf824C6G6JUdypORJwoi5S8Qze/view?usp=sharing"
+url = "https://github.com/ProfBrockway/StreamlitApp1Deploy/blob/main/TestCSVFile.csv"
 df = pd.read_csv(url)
 
 print(df.head())
